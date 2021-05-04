395f30316e7dc75c76e0507e37041c801a1e0212
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-04 05:53:32 -0500

    add events to sockets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const { createServer } = require('http')
 const { Server, Socket } = require('socket.io');
 
-const serverPeices = [
+const initialBoard = [
 	['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
 	['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
 	[null, null, null, null, null, null, null, null],
@@ -11,6 +11,8 @@ const serverPeices = [
 	['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
 	['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
 ]
+
+const rooms = {}
 
 const httpServer = createServer()
 const io = new Server(httpServer, {
@@ -22,22 +24,72 @@ const io = new Server(httpServer, {
 
 io.on('connection', socket => {
     console.log('Socket connected');
-    socket.emit('connected', 'hello from sockets')
+    socket.emit('connected')
 
-    socket.emit('init board', serverPeices)
+    socket.on('join room', (roomId)=> {
+        socket.roomId = roomId
+
+        if (!rooms[roomId]) {
+            rooms[roomId] = {
+                board: JSON.parse(JSON.stringify(initialBoard)),
+                players: [],
+                current: 0,
+            }
+        }
+
+        if (rooms[roomId].players.length >= 2) {
+            socket.emit('room full')
+            return
+        }
+
+        rooms[roomId].players.push(socket)
+        socket.join(roomId)
+
+        socket.emit('init board',  rooms[roomId].board)
+
+        if (rooms[roomId].players.length === 2) {
+            const { players } = rooms[roomId]
+            if (Math.random() > 0.5) {
+               rooms[roomId].current = 1
+               players[1].emit('white Turn')
+               players[0].emit('black Turn')
+            } else {
+                players[0].emit('white Turn')
+                players[1].emit('black Turn')
+            }
+        }
+    })
+
 
     socket.on('move', ([prev, next]) => {
-        console.log({ prev, next});
+        const { roomId } = socket
+        if (!roomId) return
+
+        const room = rooms[roomId]
         const [xNext, yNext] = next
         const [xPrev, yPrev] = prev
 
-        serverPeices[yNext][xNext] = serverPeices[yPrev][xPrev]
-        serverPeices[yPrev][xPrev] = null
-        io.emit('move', [prev, next])
+       if (!room) {
+            return
+        }
+
+        room.board[yNext][xNext] = room.board[yPrev][xPrev]
+        room.board[yPrev][xPrev] = null
+
+        io.to(roomId).emit('move', [prev, next])
+
+        room.current = room.current ? 0 : 1
+
+        room.players[room.current].emit('white Turn')
     })
 
-    socket.on('test', () => {
-        console.log('test');
+    socket.on('disconnecting', () => {
+        const socketRooms = [...socket.rooms]
+        socketRooms.forEach(room => {
+            if (!rooms[room]) return
+            rooms[room].players = rooms[room].players.filter(player => player !== socket)
+            io.to(room).emit('player left')
+        })
     })
 })
 
