5c603383f8a9351225dde25110aef3f8f8cfda08
Juan Sebastián Agudelo <juansac.me@gmail.com>
2021-05-04 02:43:13 -0500

    create sockets events

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 const { createServer } = require('http')
 const { Server, Socket } = require('socket.io');
 
+const serverPeices = [
+	['bR', 'bN', 'bB', 'bQ', 'bK', 'bB', 'bN', 'bR'],
+	['b', 'b', 'b', 'b', 'b', 'b', 'b', 'b'],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	[null, null, null, null, null, null, null, null],
+	['w', 'w', 'w', 'w', 'w', 'w', 'w', 'w'],
+	['wR', 'wN', 'wB', 'wQ', 'wK', 'wB', 'wN', 'wR'],
+]
 
 const httpServer = createServer()
 const io = new Server(httpServer, {
@@ -13,6 +23,22 @@ const io = new Server(httpServer, {
 io.on('connection', socket => {
     console.log('Socket connected');
     socket.emit('connected', 'hello from sockets')
+
+    socket.emit('init board', serverPeices)
+
+    socket.on('move', ([prev, next]) => {
+        console.log({ prev, next});
+        const [xNext, yNext] = next
+        const [xPrev, yPrev] = prev
+
+        serverPeices[yNext][xNext] = serverPeices[yPrev][xPrev]
+        serverPeices[yPrev][xPrev] = null
+        io.emit('move', [prev, next])
+    })
+
+    socket.on('test', () => {
+        console.log('test');
+    })
 })
 
 // Server
